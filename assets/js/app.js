369503b24ce4bcb07ad6a40f941e864769a9868d
Muhammad Qasim <malikqasim20051@gmail.com>
2023-10-02 02:13:53 -0700

    Home Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@ const choiceArray = document.querySelectorAll(".choice")
 
 
 
+let playButton = document.getElementById("play_button");
+let pauseButton = document.getElementById("pause_button");
 
 
 
@@ -74,7 +76,46 @@ jQuery(document).ready(function($) {
         time: 5000
     });
 
+    playButton.addEventListener("click", function() {
+        video.play();
+        playButton.classList.add('vplay');
+        pauseButton.classList.remove('vpause');
+        document.querySelector('html').classList.add('playvideo');
+      });
+      
+      pauseButton.addEventListener("click", function() {
+          video.pause();
+           playButton.classList.remove('vplay');
+        pauseButton.classList.add('vpause');
+      });
+      //Video Play Pause Control End
+      //Video Sound on & off Ctrl Start
+      let soundOff = document.getElementById("sound-off");
+      let soundOn = document.getElementById("sound-on");
+      let soundCtrl = document.querySelector(".sound-ctrl");
+      
+      soundOff.addEventListener("click", function() {
+       video.muted = true;
+       soundCtrl.classList.add('soundctrlshow');
+      });
+      
+      soundOn.addEventListener("click", function() {
+          video.muted = false;
+          soundCtrl.classList.remove('soundctrlshow'); 
+      });
+      //Video Sound on & off Ctrl End
+      video.onended = function() {
+              document.querySelector('html').classList.remove('playvideo');
+           playButton.classList.remove('vplay');
+      };
+
+
+    
+
+    
+
 
 
     
 });
+
